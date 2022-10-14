8ccaa6914bd60279280f8dee8b1d58bdf49383ab
waldivamweb21 <waldivamprogramadorweb21@gmail.com>
2022-10-14 07:04:39 -0300

    javadoc

---
+++ javadoc/src/Comentarios.java
@@ -0,0 +1,17 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // comentario em uma única linha
+
+        /*
+         * comentário multiplas linhas
+         * mais detalhado quando necessário
+         */
+    }
+         /**
+          * Estas duas estrelas é para documentação
+          */
+        public int somar (int numeroUm, int numeroDois){
+            return numeroUm + numeroDois;
+        }
+
+}
