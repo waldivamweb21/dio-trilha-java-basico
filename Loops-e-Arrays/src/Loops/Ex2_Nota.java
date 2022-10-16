70b3662be7f84186ece4916ccfd2542afa8cbdd6
waldivamweb21 <waldivamprogramadorweb21@gmail.com>
2022-10-16 18:07:12 -0300

    LoopsEArrays-ExercCamiDio

---
+++ Loops-e-Arrays/src/Loops/Ex2_Nota.java
@@ -0,0 +1,30 @@
+package Loops;
+
+import java.util.Scanner;
+
+/*
+Faça um programa que peça uma nota, entre zero e dez.
+Mostre uma mensagem caso o valor seja inválido
+e continue pedindo
+até que o usuário informe um valor válido.
+*/
+
+public class Ex2_Nota {
+    public static void main(String[] args){
+        Scanner teclado = new Scanner(System.in);
+
+        int nota;
+
+        System.out.println("Digite a sua nota: ");
+        nota = teclado.nextInt();
+
+        while(nota < 0 || nota > 10){
+                System.out.println("Nota Inválida! Digite a sua nota novamente: ");
+                nota = teclado.nextInt();
+            
+        }
+
+        teclado.close();
+        }
+    }
+
